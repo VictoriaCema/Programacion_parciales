8988b6abc9c1ef996e5e6542e6b15d99ba7552c7
Victoria Cema <victoriacerettimadrid@gmail.com>
2024-10-08 22:35:08 -0300

    Validacion de inventario vacio

--- Simulacro.py
+++ Simulacro.py
@@ -43,8 +43,8 @@ os.system("cls")
 #Entrega del programa
 #▪ La entrega se deberá realizar mediante el link al repositorio de GitHub.
 
-inventario = [["Laptop", 500000.00, 10 ], ["Silla", 14000.00, 50], ["Libro", 12000.00, 100], ["Monitor", 70000.00, 30]]
-
+inventario = []
+#["Laptop", 500000.00, 10 ], ["Silla", 14000.00, 50], ["Libro", 12000.00, 100], ["Monitor", 70000.00, 30]
 
 def validar_int(a):
     if not a.isdigit():
@@ -86,76 +86,86 @@ def agregar_producto_inventario (a, b, c):
 #producto específico por su nombre y muestre por pantalla todos los datos de
 #ese producto (nombre, precio y cantidad).
 def buscar_producto(a):
-    for i in range(len(inventario)):
-        if a == inventario[i][0]:
-            print(inventario[i])
-            return True
-    return False
-
+    if inventario != []:
+        for i in range(len(inventario)):
+            if a == inventario[i][0]:
+                print(inventario[i])
+                return True
+        return False
+    else:
+        print("Error, prinero debe ingresar productor al inventario.")
 
 #4. Ordenar inventario:
 #• Utilizar un algoritmo de ordenamiento para ordenar los productos en función
 #de su precio de manera ascendente y luego mostrar por pantalla los productos
 #ordenados. 
 def ordenar_inventario(arreglo):
-    n = len(arreglo)
+    if inventario != []:
+        n = len(arreglo)
 
-    for i in range(n-1):  
-        for j in range(n-1-i): 
-            if arreglo[j][1] > arreglo[j+1][1]:
-                arreglo[j], arreglo[j+1] = arreglo[j+1], arreglo[j]
+        for i in range(n-1):  
+            for j in range(n-1-i): 
+                if arreglo[j][1] > arreglo[j+1][1]:
+                    arreglo[j], arreglo[j+1] = arreglo[j+1], arreglo[j]
+    else:
+        print("Error, prinero debe ingresar productor al inventario.")
 
 #5. Mostrar producto más caro:
 #• Desarrollar una función que identifique y muestre el producto más caro del
 #inventario.
 def mostrar_prod_mascaro():
-    producto = ""
-    precio_caro = 0
-    
-    for i in range(len(inventario)):
-        precio_producto = inventario[i][1]
-        if precio_caro < precio_producto:
-            precio_caro = precio_producto
-            producto = inventario[i][0]
-    return producto, precio_caro
+    if inventario != []:
+        producto = ""
+        precio_caro = 0
+
+        for i in range(len(inventario)):
+            precio_producto = inventario[i][1]
+            if precio_caro < precio_producto:
+                precio_caro = precio_producto
+                producto = inventario[i][0]
+        return producto, precio_caro
+    else:
+        print("Error, prinero debe ingresar productor al inventario.")
 
 #6. Mostrar producto más barato:
 #• Desarrollar una función que identifique y muestre el producto más barato del
 #inventario.
 def mostrar_prod_masbarato():
-    producto = ""
-    precio_barato = inventario[0][1]
-    
-    for i in range(len(inventario)):
-        precio_producto = inventario[i][1]
-        if precio_barato > precio_producto:
-            precio_barato = precio_producto
-            producto = inventario[i][0]
-        
-    return producto, precio_barato
+    if inventario != []:
+        producto = ""
+        precio_barato = inventario[0][1]
+
+        for i in range(len(inventario)):
+            precio_producto = inventario[i][1]
+            if precio_barato > precio_producto:
+                precio_barato = precio_producto
+                producto = inventario[i][0]
+
+        return producto, precio_barato
+    else:
+        print("Error, prinero debe ingresar productor al inventario.")
 
 #• Mostrar productos con precio mayor a 15000.
 def mostrar_prod_precio15000():
-    producto_encontrado = False
-    productos_mayor15000 = []
-    for i in range(len(inventario)):
-        if inventario[i][1] > 15000:
-            productos_mayor15000.append(inventario[i])
-            producto_encontrado = True
-    if not producto_encontrado:
-        mensaje = "No hay productos con precio mayor a 15000"
-        return mensaje
-    return productos_mayor15000
+    if inventario != []:
+        producto_encontrado = False
+        productos_mayor15000 = []
+        for i in range(len(inventario)):
+            if inventario[i][1] > 15000:
+                productos_mayor15000.append(inventario[i])
+                producto_encontrado = True
+        if not producto_encontrado:
+            mensaje = "No hay productos con precio mayor a 15000"
+            return mensaje
+        return productos_mayor15000
+    else:
+        print("Error, prinero debe ingresar productor al inventario.")
 
-entrar = True
-while entrar:
+opcion = "juan"
+while opcion != "7":
     opcion = mostrar_menu()
-    while not validar_int(opcion) and opcion not in [1, 2, 3, 4, 5, 6, 7]:
-        opcion = input("Caracter inválido, ingrese un numero entre 1 y 7: ")
-        validar_int(opcion)
-    opcion = int(opcion)
     match opcion:
-        case 1: 
+        case "1": 
             nombre_producto = input("Ingrese el nombre del producto: ")
             precio_producto = input("Ingrese el precio del producto: ")
             while not validar_int(precio_producto):
@@ -168,13 +178,13 @@ while entrar:
                 validar_int(cant_producto)
             cant_producto = int(cant_producto)
             agregar_producto_inventario(nombre_producto, precio_producto, cant_producto)
-        case 2: 
+        case "2": 
             producto = input("Ingrese el nombre del producto que desea buscar: ")
             if not buscar_producto(producto):
                 print("Producto no encontrado.")
-        case 3: 
+        case "3": 
             ordenar_inventario(inventario)
-        case 4:
+        case "4":
             mascaro_o_masbarato = input("Si desea buscar el producto mas caro ingrese 1, si desea buscar el mas barato ingrese 2: ")
             while not validar_int(mascaro_o_masbarato) and mascaro_o_masbarato not in [1, 2]:
                 mascaro_o_masbarato = input("Caracter inválido, ingrese 1 o 2: ")
@@ -184,16 +194,18 @@ while entrar:
                 print (mostrar_prod_mascaro())
             elif mascaro_o_masbarato == 2:
                 print (mostrar_prod_masbarato())
-        case 5:
+        case "5":
             for i in range(len(mostrar_prod_precio15000())):
                 print(f"Producto: {mostrar_prod_precio15000()[i][0]}, Precio: {mostrar_prod_precio15000()[i][1]}, Stock: {mostrar_prod_precio15000()[i][2]}")
-        case 6: 
+        case "6": 
             for i in range(len(inventario)):
                 print(f'''
     Producto: {inventario[i][0]}
         precio: ${inventario[i][1]}, 
         cantidad: {inventario[i][2]}u''')
-        case 7:
-            entrar = False
+        case "7":
             print("Usted a salido del programa.")
+            
+        case _:
+            print("Error")
 
